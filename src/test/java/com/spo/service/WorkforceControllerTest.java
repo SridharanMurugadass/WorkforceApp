8f15847a8a5d53c57a7da2fc6f47e27d5074fad0
Sridharan Murugadass <sridharanbe.ece@gmail.com>
2018-11-28 22:17:04 +0530

    Update WorkforceControllerTest.java

--- src/test/java/com/spo/service/WorkforceControllerTest.java
+++ src/test/java/com/spo/service/WorkforceControllerTest.java
@@ -1,13 +1,45 @@
 package com.spo.service;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
-
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import org.junit.Assert;
+import org.junit.Before;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = WorkforceAppApplication.class)
+@SpringBootTest
 public class WorkforceControllerTest {
+
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+
+	}
+
+	@Test
+	public void task() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.post("/data").contentType(MediaType.APPLICATION_JSON)
+				.content("{ \"rooms\": [35, 21, 17], \"senior\":10, \"junior\": 6 }")
+				.accept(MediaType.APPLICATION_JSON)).andDo(print());
+	}
 
 	@Test
 	public void case1() {
